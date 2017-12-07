c151b054284950befd5638750e1e0d2731c0aae8
Wagner Nogueira <wagner@ephrom.com.br>
2017-12-07 13:24:01 -0200

    Customizando evento

--- CoreDDD.Domain/Models/Evento.cs
+++ CoreDDD.Domain/Models/Evento.cs
@@ -10,5 +10,11 @@ namespace Core.Domain.Models
     public string Nome { get; set; }
     public string DescricaoCurta { get; set; }
     public string Descricao { get; set; }
+    public DateTime DataInicio { get; set; }
+    public DateTime DataFim { get; set; }
+    public bool Gratuito { get; set; }
+    public decimal Valor { get; set; }
+    public bool Online { get; set; }
+
   }
 }
