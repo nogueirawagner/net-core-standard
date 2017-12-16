ca07bf73f7e3e466cdad3bb1f34fcf10f4e1281f
Wagner Nogueira <wagner@ephrom.com.br>
2017-12-16 20:04:48 -0200

    Implementando comands e handlers

---
+++ CoreDDD.Domain/CommandHandlers/CommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain.CommandHandlers
+{
+  public abstract class CommandHandler
+  {
+    protected void NotificarValidacoesErro(ValidationResult validationResult)
+    {
+      foreach(var erro in validationResult.Errors)
+      {
+        var error = erro.ErrorMessage;
+      }
+    }
+  }
+}
---
+++ CoreDDD.Domain/Eventos/CommandHandlers/EventoCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using Core.Domain.CommandHandlers;
+using Core.Domain.Core.Events;
+using Core.Domain.Eventos.Commands;
+using Core.Domain.Models.Eventos;
+using Eventos.IO.Domain.Eventos.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain.Eventos.CommandHandlers
+{
+  public class EventoCommandHandler : CommandHandler,
+    IHandler<RegistrarEventoCommand>,
+    IHandler<AtualizarEventoCommand>,
+    IHandler<ExcluirEventoCommand>
+  {
+
+    public EventoCommandHandler(IEventoRepository eventoRepository)
+    {
+      _eventoRepository = eventoRepository;
+    }
+
+    private readonly IEventoRepository _eventoRepository;
+
+    public void Handle(RegistrarEventoCommand message)
+    {
+      var evento = new Evento(message.Nome, message.Descricao, message.DataInicio, message.DataFim, 
+        message.Gratuito, message.Valor, message.Online, message.NomeEmpresa);
+
+      if(!evento.EhValido())
+      {
+        NotificarValidacoesErro(evento.ValidationResult);
+        return;
+      }
+
+      // TODO:
+      // Validações de negócio
+          // Organizador por registrar evento?
+
+
+      // Persistencia
+      _eventoRepository.Adicionar(evento);
+    }
+
+    public void Handle(ExcluirEventoCommand message)
+    {
+      
+    }
+
+    public void Handle(AtualizarEventoCommand message)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
