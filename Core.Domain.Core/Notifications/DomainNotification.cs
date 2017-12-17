7c700323e53a32b2f3f75a5ce2211d5282dd4d22
Wagner Nogueira <wagnogueira19@hotmail.com>
2017-12-17 20:17:47 -0200

    Implementando domain notifications para retornar mensagens de erros

---
+++ Core.Domain.Core/Notifications/DomainNotification.cs
@@ -0,0 +1,21 @@
+﻿using Core.Domain.Core.Events;
+using System;
+
+namespace Core.Domain.Core.Notifications
+{
+  public class DomainNotification : Event
+  {
+    public Guid DomainNotificationId { get; set; }
+    public string Key { get; set; } // nome do evento
+    public string Value { get; set; } // mensagem
+    public int Version { get; set; }
+
+    public DomainNotification(string key, string value)
+    {
+      DomainNotificationId = Guid.NewGuid();
+      Key = key;
+      Value = value;
+      Version = 1;
+    }
+  }
+}
---
+++ Core.Domain.Core/Notifications/IDomainNotificationHandler.cs
@@ -0,0 +1,13 @@
+﻿using Core.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain.Core.Notifications
+{
+  public interface IDomainNotificationHandler<T> : IHandler<T> where T : Message
+  {
+    bool HasNotification();
+    List<T> GetNotifications();
+  }
+}
